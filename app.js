b3b22138adccf6e68aeac5da1e8ea5d9822b8abf
Hector Valdes <contacto@hectorvaldesm.com>
2023-09-14 00:16:05 -0300

    Changed animation and fixing some stuffs

--- app.js
+++ app.js
@@ -1,97 +1,192 @@
-
-//Funcion para saber que carta debemos voltear
-let cards = document.getElementsByClassName('card');
-for (let i = 0; i < cards.length; i++) {
-  cards[i].addEventListener("click", () => { cards[i].classList.toggle('is-flipped'); })
-};
-
-var imagenes = [
-  "https://picsum.photos/id/237/100/150",
-  "https://picsum.photos/id/2/100/150",
-  "https://picsum.photos/id/30/100/150",
-  "https://picsum.photos/id/70/100/150",
-  "https://picsum.photos/id/52/100/150",
-  "https://picsum.photos/id/8/100/150",
-  "https://picsum.photos/id/6/100/150",
-  "https://picsum.photos/id/56/100/150",
-  "https://picsum.photos/id/45/100/150",
-  "https://picsum.photos/id/112/100/150",
-  "https://picsum.photos/id/82/100/150",
-  "https://picsum.photos/id/19/100/150"
+const imagenes1 = [
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/1-carta_que-1.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/1-carta_que-2.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/1-carta_que-3.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/1-carta_que-4.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/1-carta_que-5.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/1-carta_que-6.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/1-carta_que-7.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/1-carta_que-8.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/1-carta_que-9.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/1-carta_que-10.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/1-carta_que-11.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/1-carta_que-12.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/1-carta_que-13.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/1-carta_que-14.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/1-carta_que-15.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/1-carta_que-16.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/1-carta_que-17.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/1-carta_que-18.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/1-carta_que-19.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/1-carta_que-20.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/1-carta_que-21.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/1-carta_que-22.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/1-carta_que-23.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/1-carta_que-24.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/1-carta_que-25.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/1-carta_que-25.webp"
 ];
 
-//Funcion para obtener el indice de un array de manea aleatoria
-function obtenerElementoAleatorio(array) {
-  const indiceAleatorio = Math.floor(Math.random() * array.length);
-  return array[indiceAleatorio];
-}
+const imagenes2 = [
+"http://cartasporlainclusion.com/wp-content/uploads/2023/07/Bar@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Bosque-@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Calle@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Centro-cultural@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Cine@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Departamento@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Edificio-patrimonial-@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/En-la-oscuridad@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Escuela@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Festival@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Galeria-de-arte@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Estacionamiento@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Habitacion@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Lugar-sagrado@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Municipalidad@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Museo-@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Mercado-@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Pagina-web@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Parque@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Playa-@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Redes-sociales-@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Restaurante@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Teatro@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Tienda@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Zocalo-plaza-de-armas@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Zona-rural@2x.webp"
+];
 
-//Cuando se cargue el sitio se ejecuta esta funcion | asignar imagen al back
-window.addEventListener("load", () => {
-  for (let i = 0; i < cards.length; i++) {
-    let indiceArr = obtenerElementoAleatorio(imagenes);
-    document.querySelector(`.card__face--back${i + 1}`).style.backgroundImage = `url(${indiceArr})`;
-  };
-});
+const imagenes3 = [
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Dinero@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/El-acceso-no-es-prioridad-@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/El-cambio-es-dificil@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/En-necesario-hablar@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Es-necesario-entenderlo-@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Es-necesario-escucharlo@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Es-necesario-estar-en-silencio@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Es-necesario-leerlo-@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Es-necesario-movernos-@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Es-necesario-ser-fuerte-y-rapido@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Es-necesario-verlo-@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Escalones-o-suelo-irregular-@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/multitudes-@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/No-cuento-con-ayuda@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/No-hay-acceso-a-internet-@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/No-entiendo-el-lenguaje-@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/No-puedo-salir-de-casa-@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/No-se-a-quien-contactar-@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/No-se-como-hacer-esto-o-lo-hago-diferente-@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/No-se-tiene-experiencia-con-personas-con-discapacidad-@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Se-piensa-que-el-arte-de-las-personas-con-discapacidad-no-es-bueno-@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Se-piensa-que-la-discapacidad-es-vergonzosa-@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Se-piensa-que-las-personas-con-discapacidad-no-pueden-hacer-cosas-@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Se-separa-a-las-personas-con-discapacidad-@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Zocalo-plaza-de-armas@2x.webp",
+  "http://cartasporlainclusion.com/wp-content/uploads/2023/07/Tengo-poca-confianza@2x.webp"
+];
 
-//Funcionalidad del boton cambiar carta 1
-const btnCambiarCarta1 = document.querySelector('.btn__card--1--ChangeCard');
-const btnCambiarCarta2 = document.querySelector('.btn__card--2--ChangeCard');
-const btnCambiarCarta3 = document.querySelector('.btn__card--3--ChangeCard');
+    const image = document.querySelector(".card--1");
 
-btnCambiarCarta1.addEventListener('click', () => {
-  let indiceArr = obtenerElementoAleatorio(imagenes);
-  if (document.querySelector('.card--1').classList.contains("is-flipped")) {
-    document.querySelector('.card--1').classList.remove('is-flipped');
-  }
-  document.querySelector(".card__face--back1").style.backgroundImage = `url(${indiceArr})`;
-})
+    //Funcion para saber que carta debemos voltear
+    let cards = document.getElementsByClassName('card');
+    for (let i = 0; i < cards.length; i++) {
+      cards[i].addEventListener("click", () => { cards[i].classList.toggle('is-flipped'); })
+    };
+    
 
-btnCambiarCarta2.addEventListener('click', () => {
-  let indiceArr = obtenerElementoAleatorio(imagenes);
-  if (document.querySelector('.card--2').classList.contains("is-flipped")) {
-    document.querySelector('.card--2').classList.remove('is-flipped');
-  }
-  document.querySelector(".card__face--back2").style.backgroundImage = `url(${indiceArr})`;
-})
+    const arrImagesArray = [imagenes1, imagenes2, imagenes3];
 
-btnCambiarCarta3.addEventListener('click', () => {
-  let indiceArr = obtenerElementoAleatorio(imagenes);
-  if (document.querySelector('.card--3').classList.contains("is-flipped")) {
-    document.querySelector('.card--3').classList.remove('is-flipped');
-  }
-  document.querySelector(".card__face--back3").style.backgroundImage = `url(${indiceArr})`;
-})
-
-
-
-//Funcionalidad del boton mostrar carta
-const btnVerCarta1 = document.querySelector('.btn__card--1--showCard');
-const btnVerCarta2 = document.querySelector('.btn__card--2--showCard');
-const btnVerCarta3 = document.querySelector('.btn__card--3--showCard');
-
-btnVerCarta1.addEventListener('click', () => {
-  document.querySelector('.card--1').classList.toggle('is-flipped');
-})
-btnVerCarta2.addEventListener('click', () => {
-  document.querySelector('.card--2').classList.toggle('is-flipped');
-})
-btnVerCarta3.addEventListener('click', () => {
-    document.querySelector('.card--3').classList.toggle('is-flipped');
-})
-
-//Revolver TODAS las cartas
-let botonRevolver = document.querySelector('.btn__revolver__cartas');
-botonRevolver.addEventListener('click', () => {
-  let cards = document.getElementsByClassName('card');
-
-  for (let i = 0; i < cards.length; i++) {
-    let indiceArr = obtenerElementoAleatorio(imagenes);
-
-    if (document.querySelector(`.card--${i+1}`).classList.contains("is-flipped")) {
-      document.querySelector(`.card--${i+1}`).classList.remove('is-flipped');
+    //Funcion para obtener el indice de un array de manera aleatoria
+    function obtenerElementoAleatorio(array) {
+      const indiceAleatorio = Math.floor(Math.random() * array.length);
+      return array[indiceAleatorio];
+    }
+    
+    //Cuando se cargue el sitio se ejecuta esta funcion | asignar imagen al back1
+    window.addEventListener("load", () => {
+      for (let i = 0; i < cards.length; i++) {        
+          let indiceArr = obtenerElementoAleatorio(arrImagesArray[i]);      
+          document.querySelector(`.card__face--back${i + 1} img`).src = `${indiceArr}`;
+      };
+    });
+    function shuffleEffect(className){
+      setTimeout(function() {
+        document.querySelector(className).classList.add('is-shuffle');
+      }, 500);
+      setTimeout(function() {
+        document.querySelector(className).classList.remove('is-shuffle');
+      }, 3000);
     }
 
-    document.querySelector(`.card__face--back${i + 1}`).style.backgroundImage = `url(${indiceArr})`;
-  };
-})
+    
+    //Funcionalidad del boton cambiar carta
+    const btnCambiarCarta1 = document.querySelector('.btn__card--1--ChangeCard');
+    const btnCambiarCarta2 = document.querySelector('.btn__card--2--ChangeCard');
+    const btnCambiarCarta3 = document.querySelector('.btn__card--3--ChangeCard');
+    
+    btnCambiarCarta1.addEventListener('click', () => {
+      let indiceArr = obtenerElementoAleatorio(imagenes1);
+      if (document.querySelector('.card--1').classList.contains("is-flipped")) {
+        document.querySelector('.card--1').classList.remove('is-flipped');
+        shuffleEffect('.card--1');
+      }
+      document.querySelector('.card__face--back1 img').src = `${indiceArr}`;
+    })
+
+
+    btnCambiarCarta2.addEventListener('click', () => {
+      let indiceArr = obtenerElementoAleatorio(imagenes2);
+      if (document.querySelector('.card--2').classList.contains("is-flipped")) {
+        document.querySelector('.card--2').classList.remove('is-flipped');
+        shuffleEffect('.card--2');
+      }
+      document.querySelector('.card__face--back2 img').src = `${indiceArr}`;
+    })
+    
+    btnCambiarCarta3.addEventListener('click', () => {
+      let indiceArr = obtenerElementoAleatorio(imagenes3);
+      if (document.querySelector('.card--3').classList.contains("is-flipped")) {
+        document.querySelector('.card--3').classList.remove('is-flipped');
+        shuffleEffect('.card--3');
+      }
+      document.querySelector('.card__face--bac3k img').src = `${indiceArr}`;
+    })
+    
+    
+  
+    //Funcionalidad del boton mostrar carta
+    const btnVerCarta1 = document.querySelector('.btn__card--1--showCard');
+    const btnVerCarta2 = document.querySelector('.btn__card--2--showCard');
+    const btnVerCarta3 = document.querySelector('.btn__card--3--showCard');
+    
+    btnVerCarta1.addEventListener('click', (e) => {
+      document.querySelector('.card--1').classList.toggle('is-flipped');
+      btnVerCarta1.textContent = (btnVerCarta1.textContent == "Ver carta")? btnVerCarta1.textContent = "Ocultar carta": btnVerCarta1.textContent = "Ver carta";
+    })
+    btnVerCarta2.addEventListener('click', () => {
+      document.querySelector('.card--2').classList.toggle('is-flipped');
+      btnVerCarta2.textContent = (btnVerCarta2.textContent == "Ver carta")? btnVerCarta2.textContent = "Ocultar carta": btnVerCarta2.textContent = "Ver carta";
+    })
+    btnVerCarta3.addEventListener('click', () => {
+        document.querySelector('.card--3').classList.toggle('is-flipped');
+        btnVerCarta3.textContent = (btnVerCarta3.textContent == "Ver carta")? btnVerCarta3.textContent = "Ocultar carta": btnVerCarta3.textContent = "Ver carta";
+    })
+
+
+
+    //Revolver TODAS las cartas
+    let botonRevolver = document.querySelector('.btn__revolver__cartas');
+    botonRevolver.addEventListener('click', () => {
+      for (let i = 0; i < cards.length; i++) {
+        let indiceArr = obtenerElementoAleatorio(arrImagesArray[i]);
+        
+        if (document.querySelector(`.card--${i+1}`).classList.contains("is-flipped")) {
+          document.querySelector(`.card--${i+1}`).classList.remove('is-flipped');
+          shuffleEffect(`.card--${i+1}`);    
+        }
+        document.querySelector(`.card__face--back${i + 1} img`).src = `${indiceArr}`;
+      };
+    });
+
+    
+    
